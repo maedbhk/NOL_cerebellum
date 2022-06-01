3dd50908eb17708380d51a85426640c190bdabac
Maedbh King <mking86@uwo.ca>
2022-05-31 18:25:51 -0700

    add data

--- language/constants.py
+++ language/constants.py
@@ -2,11 +2,11 @@ from pathlib import Path
 import os
 
 
-#list of subj_ids for modeling
-subj_id = ['c01', 'c02', 'c03', 'c04', 'c06', 'c07', 'c08', 'c09', 'c10',
-                'c11', 'c12', 'c13', 'c14', 'c15', 'c16', 'c17', 'c18', 'p01',
-                'p02', 'p03', 'p04', 'p05', 'p07', 'p09', 'p10', 'p12', 'p13',
-                'p14', 'p15', 'p16', 'p17']
+#list of subj_ids
+subj_id = ['c01', 'c02', 'c03', 'c04', 'c05', 'c06', 'c07', 'c08', 'c09', 'c10',
+        'c11', 'c12', 'c13', 'c14', 'c15', 'c16', 'c17', 'c18', 'c19', 'p01',
+        'p02', 'p03', 'p04', 'p05', 'p06', 'p07', 'p08', 'p09', 'p10', 'p11', 
+        'p12', 'p13', 'p14', 'p15', 'p16', 'p17']
 
 
 # set base directories
